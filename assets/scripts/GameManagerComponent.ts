2bb9829e2b76958dc0d73b2ca9918538364ffe8f
Lam LV <zellitsss@gmail.com>
2024-08-10 21:30:31 +0700

    Load static data

--- assets/scripts/GameManagerComponent.ts
+++ assets/scripts/GameManagerComponent.ts
@@ -1,6 +1,7 @@
 import { _decorator, Component, JsonAsset, Node } from 'cc';
 import { GameState } from './GameState';
 import { interval } from 'rxjs';
+import { StaticData } from './StaticData';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameManagerComponent')
@@ -9,12 +10,26 @@ export class GameManagerComponent extends Component {
     @property(JsonAsset)
     initialState: JsonAsset = null;
 
+    @property(JsonAsset)
+    buildingData: JsonAsset = null;
+
+    @property(JsonAsset)
+    heroesData: JsonAsset = null;
+
     private _gameState: GameState = new GameState();
     public get gameState(): GameState {
         return this._gameState;
     }
     private set gameState(value: GameState) {
         this._gameState = value;
+    }
+
+    private _staticData: StaticData = new StaticData();
+    public get staticData(): StaticData {
+        return this._staticData;
+    }
+    private set staticData(value: StaticData) {
+        this._staticData = value;
     }
 
     private static _instance: GameManagerComponent;
@@ -32,10 +47,16 @@ export class GameManagerComponent extends Component {
     }
 
     protected start(): void {
+        this.loadStaticData();
         this.initializeState();
     }
 
-    protected initializeState() {
+    private initializeState() {
         Object.assign(this.gameState, this.initialState.json.state);
+    }
+
+    private loadStaticData() {
+        Object.assign(this.staticData, this.buildingData.json);
+        Object.assign(this.staticData, this.heroesData.json);
     }
 }
